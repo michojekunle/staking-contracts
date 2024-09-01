a84c94f00c9e78e0e0dfe7f2a6e1e8fc5f2a1017
AMD <michojekunle1@gmail.com>
2024-09-01 13:51:09 +0100

    feat: unit testing for stakingEther contract

--- test/StakingEther.ts
+++ test/StakingEther.ts
@@ -1,4 +1,7 @@
-import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import {
+  loadFixture,
+  time,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
@@ -11,7 +14,7 @@ describe("StakingEther", function () {
     const [owner, otherAccount] = await ethers.getSigners();
 
     const stakingEthContract = await ethers.getContractFactory("StakingEther");
-    const stakingEther = await stakingEthContract.deploy({ value: 100 });
+    const stakingEther = await stakingEthContract.deploy({ value: ethers.parseEther("10") });
 
     return { stakingEther, owner, otherAccount };
   }
@@ -24,14 +27,185 @@ describe("StakingEther", function () {
     });
 
     it("should revert when trying to deploy the contract with 0 ether", async function () {
-      const [owner, otherAccount] = await ethers.getSigners();
-
       const stakingEthContract = await ethers.getContractFactory(
         "StakingEther"
       );
-      expect(stakingEthContract.deploy({ value: 0 })).to.be.revertedWithCustomError(stakingEthContract, "ZeroAmountNotAllowed()");
+      expect(
+        stakingEthContract.deploy({ value: ethers.parseEther("0") })
+      ).to.be.revertedWithCustomError(
+        stakingEthContract,
+        "ZeroAmountNotAllowed"
+      );
     });
   });
 
-  describe("", function () {});
+  describe("stake", function () {
+    it("should revert with error if the owner tries to stake in the contract", async function () {
+      const { stakingEther } = await loadFixture(deployStakingEther);
+
+      expect(
+        stakingEther.stake(30, { value: ethers.parseEther("1") })
+      ).to.be.revertedWithCustomError(
+        stakingEther,
+        "OwnerCannotStakeInContract"
+      );
+    });
+
+    it("should revert with error if zero ether is passed", async function () {
+      const { stakingEther, otherAccount } = await loadFixture(
+        deployStakingEther
+      );
+
+      expect(
+        stakingEther.connect(otherAccount).stake(30, { value: ethers.parseEther("0") })
+      ).to.be.revertedWithCustomError(stakingEther, "ZeroAmountNotAllowed");
+    });
+
+    it("should revert with error if max duration is exceeded", async function () {
+      const { stakingEther, otherAccount } = await loadFixture(
+        deployStakingEther
+      );
+
+      expect(
+        stakingEther.connect(otherAccount).stake(5.6863e6, { value: ethers.parseEther("2") })
+      ).to.be.revertedWithCustomError(
+        stakingEther,
+        "MaximumStakingDurationExceeded"
+      );
+    });
+
+    it("should create new stake for user", async function () {
+      const { stakingEther, owner, otherAccount } = await loadFixture(
+        deployStakingEther
+      );
+
+      const duration = 60;
+      await stakingEther.connect(otherAccount).stake(duration, { value: ethers.parseEther("0.7") });
+      const userStakes = await stakingEther
+        .connect(otherAccount)
+        .getUserStakes();
+      const timeCreated = Math.floor(new Date().getTime() / 1000);
+
+      expect(userStakes.length).to.equal(1);
+      expect(userStakes[0].startTime).to.be.within(
+        timeCreated - 3,
+        timeCreated + 3
+      );
+      expect(userStakes[0].endTime).to.be.within(
+        timeCreated - 3,
+        timeCreated + duration + 3
+      );
+      expect(userStakes[0].isWithdrawn).to.equal(false);
+      setTimeout(
+        async () =>
+          expect(
+            await stakingEther.connect(otherAccount).getCurrentStakeBalance(0)
+          ).to.be.greaterThan(userStakes[0].amount),
+        5000
+      );
+    });
+
+    it("should revert with custom when maximum number of stakes for a user is reached", async function () {
+      const { stakingEther, owner, otherAccount } = await loadFixture(
+        deployStakingEther
+      );
+
+      await stakingEther.connect(otherAccount).stake(15, { value: ethers.parseEther("0.23") });
+      await stakingEther.connect(otherAccount).stake(23, { value: ethers.parseEther("0.23") });
+      await stakingEther.connect(otherAccount).stake(56, { value: ethers.parseEther("0.23") });
+
+      const userStakes = await stakingEther
+        .connect(otherAccount)
+        .getUserStakes();
+      expect(userStakes.length).to.equal(3);
+      expect(
+        stakingEther.connect(otherAccount).stake(62, { value: ethers.parseEther("0.25") })
+      ).to.be.revertedWithCustomError(
+        stakingEther,
+        "MaximumNumberOfStakesForUserReached"
+      );
+    });
+
+    it("should emit an event when the stake is created", async function () {
+      const { stakingEther, owner, otherAccount } = await loadFixture(
+        deployStakingEther
+      );
+
+      await expect(
+        stakingEther.connect(otherAccount).stake(200, { value: ethers.parseEther("0.5") })
+      ).to.emit(stakingEther, "StakeDeposited");
+    });
+  });
+
+  describe("withdraw", function () {
+    it("should revert with error if user stake doesn't exist", async function () {
+      const { stakingEther } = await loadFixture(deployStakingEther);
+
+      expect(stakingEther.withdraw(0)).to.be.revertedWithCustomError(
+        stakingEther,
+        "UserStakeDoesNotExist"
+      );
+    });
+
+    it("should revert with error if stake is already withdrawn", async function () {
+      const { stakingEther, otherAccount } = await loadFixture(
+        deployStakingEther
+      );
+
+      await stakingEther.connect(otherAccount).stake(10, { value: ethers.parseEther("0.2") });
+
+      setTimeout(async () => {
+        await stakingEther.connect(otherAccount).withdraw(0);
+      }, 12);
+
+      setTimeout(async () => {
+        await expect(
+          stakingEther.connect(otherAccount).withdraw(0)
+        ).to.be.revertedWithCustomError(stakingEther, "StakeAlreadyWithdrawn");
+      }, 12);
+    });
+
+    it("should revert with error if stake time has not ended", async function () {
+      const { stakingEther, otherAccount } = await loadFixture(
+        deployStakingEther
+      );
+
+      await stakingEther.connect(otherAccount).stake(10, { value: ethers.parseEther("0.2") });
+
+      expect(
+        stakingEther.connect(otherAccount).withdraw(0)
+      ).to.be.revertedWithCustomError(stakingEther, "StakeTimeHasNotEnded");
+    });
+
+    it("should withdraw staked amount with expected reward", async function() {
+        const { stakingEther, otherAccount } = await loadFixture(
+            deployStakingEther
+          );
+          let balanceBefore, balanceAfter;
+          balanceBefore = await ethers.provider.getBalance(otherAccount.address);
+
+          await stakingEther.connect(otherAccount).stake(10, { value: ethers.parseEther("0.2") });
+    
+          setTimeout(async () => {
+            await expect(stakingEther.connect(otherAccount).withdraw(0))
+            balanceAfter = await ethers.provider.getBalance(otherAccount.address);
+            expect(balanceAfter).to.be.greaterThan(balanceBefore);
+          }, 12);
+    })
+
+    it("should emit an event when the stake is withdrawn", async function () {
+      const { stakingEther, owner, otherAccount } = await loadFixture(
+        deployStakingEther
+      );
+
+      await stakingEther.connect(otherAccount).stake(10, { value: ethers.parseEther("0.2") });
+
+      setTimeout(async () => {
+        await expect(stakingEther.connect(otherAccount).withdraw(0)).to.emit(
+          stakingEther,
+          "StakeWithdrawn"
+        );
+      }, 12);
+    });
+  });
 });
